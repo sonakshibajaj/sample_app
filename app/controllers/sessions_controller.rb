d33d797ebd0aaa1af1ca5ae78715c2894581fdb8
Sonakshi Bajaj <sonakshibajaj@gmail.com>
2012-05-06 19:07:39 +0530

    Added a Sessions controller and sign in view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+  	@title = "Sign in"
+  end
+  
+  def create
+  	#@title = "Sign in"
+  end
+  
+  def destroy
+  	#@title = "Sign in"
+  end
+
+end
